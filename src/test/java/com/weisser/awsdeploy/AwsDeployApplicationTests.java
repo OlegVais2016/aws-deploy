c7f9eed562a4f6966cc5d7f6dbbb34d56f064326
Oleg Vaiser <aly9681082@gmail.com>
2021-04-04 20:15:11 +0300

    add

---
+++ src/test/java/com/weisser/awsdeploy/AwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weisser.awsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
